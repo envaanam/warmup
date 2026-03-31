d5e56bafcfc57c996908a5be5a91ade18b02996d
hari <harikrishnan.g@gmail.com>
2026-03-31 17:02:55 +0100

    Factor in the offers

--- src/main/java/com/mc/co/Checkout.java
+++ src/main/java/com/mc/co/Checkout.java
@@ -1,9 +1,17 @@
 package com.mc.co;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+/**
+ * Checkout class which does the core checkout
+ * Intentionally a singleton class
+ */
 public class Checkout {
 
+    public static final String APPLE = "apple";
+    public static final String ORANGE = "orange";
     private static Checkout INSTANCE;
 
     private Checkout() {
@@ -16,13 +24,64 @@ public class Checkout {
         return INSTANCE;
     }
 
-    public String checkout(List<String> items) {
+    /**
+     * Takes in the list of items as String and returns the total price as String
+     * @param cart - List of all items in the cart
+     * @return - final total price after all the discounts
+     */
+    public String checkout(List<String> cart) {
         float val = 0.00f;
-        if(!(items == null || items.isEmpty())) {
-            long sumInPence = items.stream().map(String::toLowerCase).map(i -> Products.getInstance().getAllItems().get(i)).reduce(0, Integer::sum);
+
+        if(!(cart == null || cart.isEmpty())) {
+            // Total Price before the offers
+            long sumInPence = cart.stream().map(String::toLowerCase).map(i -> Products.getInstance().getAllItems().get(i)).reduce(0, Integer::sum);
+
+            // Factor in the offers
+
+            Map<String, Integer> itemCount = cart.stream().collect(Collectors.toMap(k -> k, k -> 1, Integer::sum));
+            int applesToBeDiscounted = itemCount.containsKey(APPLE) ? numberOfDiscountedItemsForBuy1Get1(itemCount.get(APPLE)) : 0;
+            int orangesToBeDiscounted = itemCount.containsKey(ORANGE) ? numberOfDiscountedItemsFor3For2(itemCount.get(ORANGE)) : 0;
+
+            long applesOfferInPence = (long) applesToBeDiscounted *  Products.getInstance().getAllItems().get(APPLE);
+            long orangesOfferInPence = (long) orangesToBeDiscounted * Products.getInstance().getAllItems().get(ORANGE);
+
+            sumInPence = sumInPence - applesOfferInPence - orangesOfferInPence;
             val = (float) sumInPence / 100;
+
         }
 
         return String.format("£%.2f", val);
+    }
+
+    /**
+     * This can be refactored to be a single line like below
+     * "return numberOfItems / 2"
+     * provided if the access is restricted within this class using private.
+     * However, I have made this public to enable testing
+     * @param numberOfItems - number of items that qualifies for buy 1, get 1
+     * @return number of items to be discounted
+     */
+    public static int numberOfDiscountedItemsForBuy1Get1(int numberOfItems) {
+        int itemsToDiscount = 0;
+        if(numberOfItems > 0 ) {
+            itemsToDiscount = numberOfItems / 2;
+        }
+        return itemsToDiscount;
+    }
+
+    /**
+     * This can be refactored to be a single line like below
+     * "return numberOfItems / 3"
+     * provided if the access is restricted within this class using private.
+     * However, I have made this public to enable testing
+     * @param numberOfItems - number of items that qualifies for this offer
+     * @return - number of items to be discounted
+     */
+    public static int numberOfDiscountedItemsFor3For2(int numberOfItems) {
+        int itemsToDiscount = 0;
+        if(numberOfItems > 0) {
+            itemsToDiscount = numberOfItems / 3;
+        }
+        return itemsToDiscount;
     }
 }
--- src/test/java/com/mc/co/CheckoutTest.java
+++ src/test/java/com/mc/co/CheckoutTest.java
@@ -2,6 +2,8 @@ package com.mc.co;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -23,13 +25,25 @@ class CheckoutTest {
     @Test
     void checkout() {
         List<String> cartItems = Arrays.asList("orange", "apple", "apple", "apple");
-        assertEquals("£2.05", Checkout.getInstance().checkout(cartItems));
+        assertEquals("£1.45", Checkout.getInstance().checkout(cartItems));
     }
 
     @Test
     void checkoutScenario2() {
-        List<String> cartItems = Arrays.asList("orange", "orange", "apple", "apple");
-        assertEquals("£1.70", Checkout.getInstance().checkout(cartItems));
+        List<String> cartItems = Arrays.asList("orange", "orange", "orange", "apple", "apple");
+        assertEquals("£1.10", Checkout.getInstance().checkout(cartItems));
+    }
+
+    @Test
+    void checkoutScenarioWithOnlyOranges() {
+        List<String> cartItems = Arrays.asList("orange", "orange", "orange");
+        assertEquals("£0.50", Checkout.getInstance().checkout(cartItems));
+    }
+
+    @Test
+    void checkoutScenarioWithOnlyApples() {
+        List<String> cartItems = Arrays.asList("apple", "apple", "apple");
+        assertEquals("£1.20", Checkout.getInstance().checkout(cartItems));
     }
 
     @Test
@@ -42,4 +56,17 @@ class CheckoutTest {
     void testNull() {
         assertEquals("£0.00", Checkout.getInstance().checkout(null));
     }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0:0", "1:0", "2:1", "3:1", "51:25"}, delimiter = ':')
+    void testNumberOfDiscountedItemsForBuy1Get1WithNItems(int input, int expected) {
+        assertEquals(expected, Checkout.numberOfDiscountedItemsForBuy1Get1(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0:0", "2:0", "3:1", "5:1", "10:3"}, delimiter = ':')
+    void testNumberOfDiscountedItemsFor3For2WithNItems(int input, int expected) {
+        assertEquals(expected, Checkout.numberOfDiscountedItemsFor3For2(input));
+    }
+
 }
